3b5088331bceca5ce1f3d7a8c18ce0f12407809a
원형 <147372294+blissyou@users.noreply.github.com>
2025-02-24 15:51:00 +0900

    spring security 설정 완료, h2console 무시, 메모리 데이터 베이스 설정

---
+++ src/main/java/org/example/auth/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package org.example.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig{
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web-> web.ignoring().requestMatchers("/h2-console/**","/favicon.ico"));
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+
+    }
+
+
+}
